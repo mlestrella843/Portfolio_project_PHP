fc988528a6f087a183ed03ddb7d338cd74184a1b
mlestrella843 <marial.estrella.843@gmail.com>
2023-05-16 17:06:51 -0600

    Integrating Bootstrap

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php  include('header.php');  ?>
 
+            <div class="p-5 bg-light">
+                <div class="container">
+                <h1 class="display-3">Welcome!</h1>
+                <p class="lead">This is my Portfolio.</p>
+                <hr class="my-2">
+                <p>More info</p>    
+            </div>
+        </div>
+    </div>
 
-    <h1>Welcome to my Portfolio in PHP</h1>
+
     
 <?php include('footer.php');  ?>
